2a2856c0206eca6802c690932de2235db8a7c02a
Andre Lael <andrelael.contato@gmail.com>
2022-02-14 16:33:02 -0400

    exercicios

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -21,4 +21,30 @@ class siteController extends Controller
 
         return view('bemvindo',$data);
     }
+
+    public function exercicios1() {
+
+        $pessoas = [
+            'image' => 'https://i.pravatar.cc/150?img='.rand(0,50),  
+                'nome' => 'Alessandro', 
+                'birth' => '29/01/90', 
+                'idade' => 29
+  
+            //   [ 
+            //     'image' => 'https://i.pravatar.cc/150?img='.rand(0,50), 
+            //     'nome' => 'Bonieky', 
+            //     'birth' => '10/11/1933', 
+            //     'idade' => 90],
+  
+            //   [ 
+            //     'image' => 'https://i.pravatar.cc/150?img='.rand(0,50), 
+            //     'nome' => 'Maria', 
+            //     'birth' => '01/01/2020', 
+            //     'idade' => 2]
+        ];
+
+        $dados['pessoas'] = $pessoas;
+    
+        return view('exercicios1', $dados);
+    }
 }
